6cc84c952fee9fca783179b1ee622b059fe5557a
Omri Harel <omriharel@users.noreply.github.com>
2024-05-14 18:31:42 +0300

    fix naming issue with contracts

--- builder/constants.py
+++ builder/constants.py
@@ -1,3 +1,11 @@
 class Constants:
     base_repo_url = "https://github.com/omriharel/poeladder-item-info"
     base_branch = "master"
+
+    all_unique_contracts = [
+        "Breaking the Unbreakable",
+        "The Slaver King",
+        "Heart of Glory",
+        "Death to Darnaw",
+        "The Twins",
+    ]
